4d9bd13fef6defa8918b454ea58d69c40eeef8bb
Yuji Takaesu <taka.esu.user@gmail.com>
2014-05-31 01:18:32 +0900

    implements bonus drink total amount

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
+  BONUS_CONDITION_NUM = 3
+
   def self.total_count_for(amount)
-    0
+    count_for(amount)
+  end
+
+  def self.count_for(num, total=0)
+    return total + num if num < BONUS_CONDITION_NUM
+
+    quotient, rest = num.divmod(BONUS_CONDITION_NUM)
+    total += quotient * BONUS_CONDITION_NUM
+    count_for(quotient + rest, total)
   end
 end
