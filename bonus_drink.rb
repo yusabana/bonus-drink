71d911fd3f281273c605858b8a29386f136d047c
Yuji Takaesu <taka.esu.user@gmail.com>
2014-05-31 22:57:14 +0900

    refactoring recursive implement and add loop implement

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,15 +1,33 @@
 class BonusDrink
   BONUS_CONDITION_NUM = 3
 
-  def self.total_count_for(num)
-    count_for(num)
-  end
-
-  def self.count_for(num, total=0)
-    return total + num if num < BONUS_CONDITION_NUM
-
+  # 11/3  3...2    3*3 = 9
+  # 5/3   1...2    3*1 = 3
+  # 3/3   1...0    3*1 = 3
+  #                      1
+  #                     16
+  def self.total_count_for(num, total=0)
     quotient, rest = num.divmod(BONUS_CONDITION_NUM)
     total += quotient * BONUS_CONDITION_NUM
-    count_for(quotient + rest, total)
+    num < BONUS_CONDITION_NUM ? total + num : total_count_for(quotient + rest, total)
   end
+
+  # 3つずつ飲むので、3つずつ計算してボーナス分を足していく
+  def self.total_count_for_while(amount)
+    rest_num = amount
+    dranks = 0
+
+    while rest_num >= 3
+      dranks += 3
+      rest_num -= 3
+      rest_num += 1
+    end
+
+    dranks + rest_num
+  end
+end
+
+# 直接実行する($0 == __FILE__ と条件を入れているので、他のファイルからバレる時は実行されないが、このファイルを直接 % ruby bonus_drink.rb とやると実行される)
+if $0 == __FILE__
+  puts BonusDrink.total_count_for(100)
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,14 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
-  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
-  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
-  specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
+  describe "#total_count_for" do
+    specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+    specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+    specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+    specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
+    specify { expect(BonusDrink.total_count_for(1000)).to eq 1499 }
+  end
+
+  describe "#total_count_for_while" do
+    specify { expect(BonusDrink.total_count_for_while(0)).to eq 0 }
+    specify { expect(BonusDrink.total_count_for_while(3)).to eq 4 }
+    specify { expect(BonusDrink.total_count_for_while(11)).to eq 16 }
+    specify { expect(BonusDrink.total_count_for_while(100)).to eq 149 }
+    specify { expect(BonusDrink.total_count_for_while(1000)).to eq 1499 }
+  end
 end
-
-
-# 11/3  3...2    3*3 = 9
-# 5/3   1...2    3*1 = 3
-# 3/3   1...0    3*1 = 3
-#                      1
